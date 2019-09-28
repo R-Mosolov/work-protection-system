47a2344ce432107c5e76751103edfd10e0768fe8
R-Mosolov <r.v.mosolov@ya.ru>
2019-09-28 10:47:16 +0300

    Added a map of place (Yandex.Map) to worker profile

--- src/pages/worker/site-content/site-content.js
+++ src/pages/worker/site-content/site-content.js
@@ -5,24 +5,30 @@ import WorkerImage from './img/engineer.svg';
 
 const SiteContent = () => {
   return (
-    <div className="login d-flex justify-content-center align-items-center">
-      <div className="card mb-3" style={{maxWidth: '540px'}}>
-        <div className="row no-gutters">
-          <div className="col-md-4">
-            <img src={WorkerImage} className="card-img" alt="Фото сотрудника"/>
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">Иванов Иван Иванович</h5>
-              <p className="card-text">Руководитель отдела инженеров в филиале г. Казань компании "Russian Company"</p>
-              <p className="card-text"><small className="text-muted">Последнее обновление данных</small></p>
-
-              {/*<h5>Состояние работника</h5>*/}
-              {/*<p className="card-text">Уровень трезвости: нормальный</p>*/}
-              {/*<p className="card-text">Удовлетворенность коллективом: нормальный</p>*/}
+    <div>
+      <div className="login d-flex flex-column justify-content-center align-items-center">
+        <div className="card mb-3" style={{maxWidth: '540px'}}>
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <img src={WorkerImage} className="card-img" alt="Фото сотрудника"/>
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Иванов Иван Иванович</h5>
+                <p className="card-text">Руководитель отдела инженеров в филиале г. Казань компании "Russian Company"</p>
+              </div>
             </div>
           </div>
         </div>
+        <div className="place-title d-flex justify-content-start">
+          <h5>Местоположение работника:</h5>
+        </div>
+        <iframe
+            src="https://yandex.ru/map-widget/v1/?um=constructor%3Aae9a082ffbe204f5ae839c683e3c22939e7c962bebb089c7e6e0390cefb41701&amp;source=constructor"
+            width="540"
+            height="400"
+            frameBorder="0"
+            className="border rounded" />
       </div>
     </div>
   );

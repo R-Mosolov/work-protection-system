2ac25fd951c4825443ba43bc5b6c47d99df51c44
R-Mosolov <r.v.mosolov@ya.ru>
2019-10-01 11:28:11 +0300

    Deploing on Google Firebase

---
+++ build/precache-manifest.35f773ff814749571eed6557999e6080.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23270860981ab1802468b25a13fa4912",
+    "url": "/index.html"
+  },
+  {
+    "revision": "409e937c3d8f16985193",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6602a26b680d9305c651",
+    "url": "/static/css/main.88d5f262.chunk.css"
+  },
+  {
+    "revision": "409e937c3d8f16985193",
+    "url": "/static/js/2.5023744b.chunk.js"
+  },
+  {
+    "revision": "6602a26b680d9305c651",
+    "url": "/static/js/main.0e7ffb90.chunk.js"
+  },
+  {
+    "revision": "9f6421af0f6f2b853fb2",
+    "url": "/static/js/runtime-main.78869bc5.js"
+  },
+  {
+    "revision": "f0f0e53e78823e27929a2e48c2cab936",
+    "url": "/static/media/companionship.f0f0e53e.svg"
+  },
+  {
+    "revision": "8d8a4a337728cf01140dc09630612347",
+    "url": "/static/media/configuration.8d8a4a33.svg"
+  },
+  {
+    "revision": "e631344b2013a385b6abcd4e37421f98",
+    "url": "/static/media/employee.e631344b.svg"
+  },
+  {
+    "revision": "13d99d8e33ec40ef3222e4f042426333",
+    "url": "/static/media/engineer.13d99d8e.svg"
+  },
+  {
+    "revision": "57a8beb632c4951886274b6b7b889d97",
+    "url": "/static/media/enterprise.57a8beb6.svg"
+  },
+  {
+    "revision": "05a926f25b2e028cd5e147ceba820928",
+    "url": "/static/media/heart-rate.05a926f2.svg"
+  },
+  {
+    "revision": "01bcf565b58d9e9cc85134edc085e581",
+    "url": "/static/media/temperature.01bcf565.svg"
+  }
+]);
